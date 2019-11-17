633d07ac56e41f1582a8e858671638a666423488
Suvendu Bera <suvendu0607@gmail.com>
2019-11-17 08:46:37 +0530

    first commit

---
+++ kits.CollegeMg/Kits.CollegeMg.Web.UI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Kits.CollegeMg.Web.UI.Startup))]
+namespace Kits.CollegeMg.Web.UI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
